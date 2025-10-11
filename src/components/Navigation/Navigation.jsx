b4c0f9c2342865819ed731ba083a658eb99ce333
vaivanor <vaiva.noraityte@gmail.com>
2025-10-11 20:40:44 +0300

    feat(components): add hamburger menu

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,21 +1,32 @@
-import style from "./Navigation.module.css";
+import style from "./Navigation.module.scss";
 import { NavLink } from "react-router-dom";
+import menu from "../../assets/menu.svg";
+import menuClose from "../../assets/menu-close.svg";
+import { useState } from "react";
 
 export const Navigation = () => {
   const getClass = ({ isActive }) => {
     return isActive ? style.active : "";
   };
 
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => setIsOpen(!isOpen);
+
   return (
-    <nav className={style.container}>
-      <div>
+    <>
+      <button className={style.menu} onClick={toggleMenu}>
+        <img className={style.img} src={isOpen ? menuClose : menu} alt="Menu" />
+      </button>
+
+      <nav className={`${style.container} ${isOpen ? style.open : ""}`}>
         <NavLink to={"/"} className={getClass}>
           Home
         </NavLink>
         <NavLink to={"/restaurants"} className={getClass}>
           Restaurants
         </NavLink>
-      </div>
-    </nav>
+      </nav>
+    </>
   );
 };
