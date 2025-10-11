7352150eacbbc18c364513d9a2b8c38afdb97860
vaivanor <vaiva.noraityte@gmail.com>
2025-10-11 21:01:52 +0300

    test(page): test font sizes, weight

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,10 @@
 import { PageWrapper } from "../components/PageWrapper/PageWrapper.jsx";
 
-export const Home = () => <PageWrapper></PageWrapper>;
+export const Home = () => (
+  <PageWrapper>
+    <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit. </h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+    <h2>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</h2>
+    <h3>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</h3>
+  </PageWrapper>
+);
