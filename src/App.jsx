2d7ff56080a0baf546edce641f9dfbbcf7182490
vaivanor <vaiva.noraityte@gmail.com>
2025-10-11 17:32:13 +0300

    feat(styles): add sccs, variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import { Footer } from "./components/Footer/Footer";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
