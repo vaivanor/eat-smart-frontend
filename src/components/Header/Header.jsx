06259b59078a8841c55ad0089284e28d26acc2cc
vaivanor <vaiva.noraityte@gmail.com>
2025-10-11 20:41:19 +0300

    feat(components): add header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import { Navigation } from "../Navigation/Navigation.jsx";
+import { Link } from "react-router-dom";
+
+import style from "./Header.module.scss";
+import logo from "../../assets/logo.svg";
+
+export const Header = () => {
+  return (
+    <header className={style.header}>
+      <Link to={"/"}>
+        <img className={style.img} src={logo} alt="EatSmart logo." />
+      </Link>
+      <Navigation />
+    </header>
+  );
+};
--- src/components/PageWrapper/PageWrapper.jsx
+++ src/components/PageWrapper/PageWrapper.jsx
@@ -1,9 +1,11 @@
 import style from "./PageWrapper.module.css";
 import { Footer } from "../Footer/Footer.jsx";
+import { Header } from "../Header/Header.jsx";
 
 export const PageWrapper = ({ children }) => {
   return (
     <div className={style.container}>
+      <Header />
       <main className={style.mainContainer}>{children}</main>
       <Footer />
     </div>
