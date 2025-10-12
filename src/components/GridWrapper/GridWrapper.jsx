1722602d3823a24a6a1df3882680352987a6ad36
vaivanor <vaiva.noraityte@gmail.com>
2025-10-12 15:26:30 +0300

    feat(components): create grid wrapper

---
+++ src/components/GridWrapper/GridWrapper.jsx
@@ -0,0 +1,25 @@
+import style from "./GridWrapper.module.css";
+import { useMemo } from "react";
+
+export const GridWrapper = ({ children, type, typeTheme, columns = 2 }) => {
+  const typeClass = useMemo(() => {
+    return {
+      left: style.left,
+      center: style.center,
+    }[type];
+  }, [type]);
+  const typeClassTheme = useMemo(() => {
+    return {
+      light: style.light,
+      dark: style.dark,
+    }[typeTheme];
+  }, [typeTheme]);
+  return (
+    <div
+      className={`${style.container} ${typeClass} ${typeClassTheme}`}
+      style={{ "--columns": columns }}
+    >
+      {children}
+    </div>
+  );
+};
