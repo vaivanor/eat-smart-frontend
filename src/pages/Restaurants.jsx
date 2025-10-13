d86e25f42d5c450224a7e900dc5f69cf777d2651
vaivanor <vaiva.noraityte@gmail.com>
2025-10-13 10:35:44 +0300

    feat(pages): add fetchData call

--- src/pages/Restaurants.jsx
+++ src/pages/Restaurants.jsx
@@ -2,8 +2,29 @@ import { BackgroundWrapper } from "../components/BackgroundWrapper/BackgroundWra
 import { PageWrapper } from "../components/PageWrapper/PageWrapper.jsx";
 import bg from "../assets/background/bg.jpeg";
 import { GridWrapper } from "../components/GridWrapper/GridWrapper.jsx";
+import { Loader } from "../components/Loader/Loader.jsx";
+import { useState, useEffect } from "react";
+import { useAppContext } from "../store/AppContext.jsx";
+import { fetchData } from "../utils/fetchData.js";
 
 export const Restaurants = () => {
+  const [restaurants, setRestaurants] = useState([]);
+  const { isLoading, setIsLoading } = useAppContext();
+
+  useEffect(() => {
+    fetchData({
+      endpoint: "/restaurants",
+      method: "GET",
+      setIsLoading,
+      onSuccess: (result) => {
+        setRestaurants(result.data);
+      },
+      onError: (error) => {
+        console.error("Error:", error);
+      },
+    });
+  }, []);
+
   return (
     <PageWrapper>
       <BackgroundWrapper src={bg}>
@@ -12,7 +33,19 @@ export const Restaurants = () => {
         </div>
       </BackgroundWrapper>
       <GridWrapper columns={1}>
-        <h1>Restaurants</h1>
+        <div>
+          {isLoading ? (
+            <Loader />
+          ) : (
+            <div>
+              {restaurants.map((restaurant) => (
+                <div key={restaurant._id}>
+                  <h3>{restaurant.name}</h3>
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
       </GridWrapper>
     </PageWrapper>
   );
