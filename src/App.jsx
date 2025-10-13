184268ce30062684fd100c3af94a36248505f1f7
vaivanor <vaiva.noraityte@gmail.com>
2025-10-13 09:00:12 +0300

    feat: add restaurants route

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
+import { Restaurants } from "./pages/Restaurants";
 
 function App() {
   return (
     <>
       <Routes>
         <Route Component={Home} path="/" />
+        <Route Component={Restaurants} path="/restaurants" />
       </Routes>
     </>
   );
