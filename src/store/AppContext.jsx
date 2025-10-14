adf24001f49c88545f7f7219303cf7ae7fd607a5
vaivanor <vaiva.noraityte@gmail.com>
2025-10-14 12:38:02 +0300

    feat(store): add handle log in / out

--- src/store/AppContext.jsx
+++ src/store/AppContext.jsx
@@ -1,13 +1,45 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 
 const AppContext = createContext();
 
 export const AppProvider = ({ children }) => {
   const [isLoading, setIsLoading] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem("accessToken");
+    setIsLoggedIn(!!token);
+  }, []);
+
+  const handleLogin = (token) => {
+    localStorage.setItem("accessToken", token);
+    setIsLoggedIn(true);
+  };
+
+  const handleLogout = async () => {
+    try {
+      await fetch("http://localhost:3000/logout", {
+        method: "POST",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+        },
+      });
+
+      localStorage.removeItem("accessToken");
+      setIsLoggedIn(false);
+    } catch (error) {
+      console.error("Logout error:", error);
+    }
+  };
 
   const value = {
     isLoading,
     setIsLoading,
+    isLoggedIn,
+    handleLogin,
+    handleLogout,
   };
 
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
