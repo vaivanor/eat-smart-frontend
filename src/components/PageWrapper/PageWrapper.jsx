5040dfb9c8a4be841b2fd9695abf3306b87157bd
vaivanor <vaiva.noraityte@gmail.com>
2025-10-15 11:12:27 +0300

    fix(components): update page wrapper

--- src/components/PageWrapper/PageWrapper.jsx
+++ src/components/PageWrapper/PageWrapper.jsx
@@ -2,11 +2,21 @@ import style from "./PageWrapper.module.scss";
 import { Footer } from "../Footer/Footer.jsx";
 import { Header } from "../Header/Header.jsx";
 
-export const PageWrapper = ({ children }) => {
+export const PageWrapper = ({ children, backgroundSrc }) => {
+  const backgroundStyle = backgroundSrc
+    ? { backgroundImage: `url(${backgroundSrc})` }
+    : {};
   return (
     <div className={style.container}>
       <Header />
-      <main className={style.mainContainer}>{children}</main>
+      <main
+        className={`${style.mainContainer} ${
+          backgroundSrc ? style.background : ""
+        }`}
+        style={backgroundStyle}
+      >
+        {children}
+      </main>
       <Footer />
     </div>
   );
