1d0ab9f1e56caa7315b7c5faa4cc14d58689d5a9
vaivanor <vaiva.noraityte@gmail.com>
2025-10-15 11:56:43 +0300

    feat(pages): update sign in, add input components styled link

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -9,6 +9,16 @@ import { Modal } from "../components/Modal/Modal.jsx";
 import { Form } from "../components/Form/Form.jsx";
 import { GridWrapper } from "../components/GridWrapper/GridWrapper.jsx";
 import bg from "../assets/background/bg.jpeg";
+import { Input } from "../components/Input/Input.jsx";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+const StyledLink = styled(Link)`
+  color: #feb161;
+  &:hover {
+    color: #e2944d;
+  }
+`;
 
 export const SignIn = () => {
   const [email, setEmail] = useState("");
@@ -53,30 +63,30 @@ export const SignIn = () => {
           <h2>Sign In</h2>
         </GridWrapper>
         <GridWrapper columns={1}>
-          <div>
-            <label>Email:</label>
-            <input
-              type="text"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-          </div>
-          <div>
-            <label>Password:</label>
-            <input
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-          </div>
+          <Input
+            id="emailSignIn"
+            label="Email"
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <Input
+            id="passwordSignIn"
+            label="Password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </GridWrapper>
 
         <GridWrapper columns={1} type="center">
           <div>
-            <Button text="Submit" type="primary" onClick={handleSubmit} />
+            <Button text="Sign In" type="primary" onClick={handleSubmit} />
           </div>
+          <p style={{ textAlign: "center" }}>
+            Don't have an account?{" "}
+            <StyledLink to="/sign-up">Sign Up</StyledLink>
+          </p>
         </GridWrapper>
       </Form>
 
