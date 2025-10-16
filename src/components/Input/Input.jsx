a3e44db43655255158ae850589e01fd42b801fdf
vaivanor <vaiva.noraityte@gmail.com>
2025-10-16 16:49:23 +0300

    feat(components) add details for input

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,9 +1,20 @@
 import style from "./Input.module.scss";
 
-export const Input = ({ id, label, type, value, onChange, error }) => {
+export const Input = ({
+  id,
+  label,
+  type,
+  value,
+  onChange,
+  error = "",
+  details = "",
+}) => {
   return (
     <div className={style.container}>
-      <label htmlFor={id}>{label}</label>
+      <label htmlFor={id}>
+        {label} <span> {details}</span>
+      </label>
+
       <input id={id} type={type} value={value} onChange={onChange} />
       {error && <p className={style.error}>{error}</p>}
     </div>
