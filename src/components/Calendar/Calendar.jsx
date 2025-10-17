7762f47fa43e7f41fc6dcf652b99049127261bab
vaivanor <vaiva.noraityte@gmail.com>
2025-10-17 18:14:01 +0300

    feat(components): create calendar for reservation

---
+++ src/components/Calendar/Calendar.jsx
@@ -0,0 +1,36 @@
+import style from "./Calendar.module.scss";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { useState } from "react";
+
+export const Calendar = ({
+  id,
+  label,
+  details,
+  workingHours,
+  selectedDate,
+  onChange,
+}) => {
+  const allowedDays = Object.keys(workingHours);
+
+  const isDateAllowed = (date) => {
+    const dayName = date.toLocaleDateString("en-US", { weekday: "long" });
+    return allowedDays.includes(dayName);
+  };
+
+  return (
+    <div className={style.container}>
+      <label htmlFor={id}>
+        {label} <span> {details}</span>
+      </label>
+      <DatePicker
+        selected={selectedDate || new Date()}
+        onChange={onChange}
+        minDate={new Date()}
+        maxDate={new Date(new Date().setMonth(new Date().getMonth() + 3))}
+        filterDate={isDateAllowed}
+        dateFormat="yyyy-MM-dd"
+      />
+    </div>
+  );
+};
