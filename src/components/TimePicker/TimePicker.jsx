a443e421c940f8e40a90d670a7e2cb525e9ed90e
vaivanor <vaiva.noraityte@gmail.com>
2025-10-17 21:18:44 +0300

    feat(components): create time picker

---
+++ src/components/TimePicker/TimePicker.jsx
@@ -0,0 +1,42 @@
+import style from "./TimePicker.module.scss";
+
+export const TimePicker = ({
+  id,
+  label,
+  details,
+  times = [],
+  onChange,
+  selectedTime,
+  error = "",
+}) => {
+  const isDisabled = times.length === 0;
+
+  return (
+    <div className={style.container}>
+      <label htmlFor={id}>
+        {label} <span>{details}</span>
+      </label>
+      <select
+        id={id}
+        value={selectedTime}
+        onChange={onChange}
+        disabled={isDisabled}
+      >
+        <option value="">{isDisabled ? "Loading..." : "Select time"}</option>
+
+        {!isDisabled &&
+          times.map((time, index) => (
+            <option key={index} value={time}>
+              {time}
+            </option>
+          ))}
+      </select>
+
+      {isDisabled && (
+        <p className={style.error}>
+          {error || "Select a date and number of people first."}
+        </p>
+      )}
+    </div>
+  );
+};
