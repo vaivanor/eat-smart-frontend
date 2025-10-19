f0af919ebe85903b4db204b8874416145712788e
vaivanor <vaiva.noraityte@gmail.com>
2025-10-19 11:05:03 +0300

    feat(components): add useEffect to PageWrapper

--- src/components/PageWrapper/PageWrapper.jsx
+++ src/components/PageWrapper/PageWrapper.jsx
@@ -1,11 +1,17 @@
 import style from "./PageWrapper.module.scss";
 import { Footer } from "../Footer/Footer.jsx";
 import { Header } from "../Header/Header.jsx";
+import { useEffect } from "react";
 
 export const PageWrapper = ({ children, backgroundSrc }) => {
   const backgroundStyle = backgroundSrc
     ? { backgroundImage: `url(${backgroundSrc})` }
     : {};
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <div className={style.container}>
       <Header />
